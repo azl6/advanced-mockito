2b51118b8a39d74d449e9f77c9bf7007f0014eb5
Alex <alex.rodrigues23997@gmail.com>
2022-05-22 15:13:11 -0300

    Initial commit

---
+++ src/test/java/com/azold6/mockitointro/MockitoIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azold6.mockitointro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
